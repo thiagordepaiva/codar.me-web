e3257fb1ede5e27b2edfe0fc55fe27ff243f2c21
Thiago Paiva <thiago.rodriguesdepaiva@gmail.com>
2023-04-18 19:32:57 -0300

    feat: layout inicial Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,41 @@
 import * as React from "react";
+import styled from "styled-components";
 
-import { Box, Button, useAuth } from "~/components";
+import { th, Title } from "~/components";
+
+const Container = styled("div")`
+  display: flex;
+  flex: 1;
+`;
+
+const Menu = styled("aside")`
+  background: ${th.color("black")};
+  padding: ${th.space(2)}px;
+`;
+
+const Main = styled("main")`
+  flex: 1;
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+const Layout = ({ children }) => (
+  <Container>
+    <Menu>Menu</Menu>
+    <Main>{children}</Main>
+  </Container>
+);
 
 export const Dashboard = () => {
-  const [auth, { logout }] = useAuth();
+  //const [auth, { logout }] = useAuth();
 
   return (
-    <Box flex={1} flexBox="column" center>
+    /*<Box flex={1} flexBox="column" center>
       Olá {auth.user.name}, você esta logado
       <Button onClick={logout}>Sair</Button>
-    </Box>
+    </Box>*/
+    <Layout>
+      <Title>Dashboard</Title>
+    </Layout>
   );
 };
